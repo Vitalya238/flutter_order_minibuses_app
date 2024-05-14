432ac9bbf5a350aaf0f14c3a0edb84d6c3706f86
BOSS <vitalya@DivineCreation.local>
2024-05-14 17:55:24 +0300

    expansionTile FAQ CLIENT

--- kp/lib/views/help_screen.dart
+++ kp/lib/views/help_screen.dart
@@ -1,19 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:kp/models/FAQ.dart';
+import 'package:kp/services/faq_handler.dart';
+import 'package:provider/provider.dart';
+import 'package:kp/services/database_notifier.dart';
 
-class HelpScreen extends StatelessWidget {
+class HelpScreen extends StatefulWidget {
+  @override
+  _HelpScreenState createState() => _HelpScreenState();
+}
+
+class _HelpScreenState extends State<HelpScreen> {
+  late Future<List<FAQ>> _faqsFuture;
+
+
+  @override
+  void initState() {
+    super.initState();
+    _faqsFuture = _getAllFAQs();
+  }
+
+  Future<List<FAQ>> _getAllFAQs() async {
+    final dbHelper = Provider.of<DatabaseNotifier>(context, listen: false).databaseHelper;
+    await dbHelper.init();
+    FAQHandler faqHandler = FAQHandler(dbHelper.db);
+
+    return faqHandler.getAllFAQs();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.amberAccent,
         title: const Text(
-          'Help',
+          'FAQs',
           style: TextStyle(fontWeight: FontWeight.w600),
         ),
       ),
-      body: const Center(
-        child: Text('Help screen'),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _buildFAQsList(),
+          ],
+        ),
       ),
     );
   }
+
+  Widget _buildFAQsList() {
+    return Expanded(
+      child: FutureBuilder(
+        future: _faqsFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Ошибка: ${snapshot.error}'));
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                final data = snapshot.data![index];
+                return ExpansionTile(
+                  title: Text('question: ${data.question}'),
+                  children: [
+                    Text('answer: ${data.answer}'),
+                  ],
+                );
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+
 }
